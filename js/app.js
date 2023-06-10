594eef8407feddae7ce3789faabed0ecc975831e
‘David <dsegura07@yahoo.com>
2023-06-09 22:57:13 -0700

    question 3 done

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-  let addedNumbers = a + b;
-  let addAnswer = `The sum of ${a} and ${b} is ${addedNumbers}.`;
-  return [addedNumbers, addAnswer];
+    let addedNumbers = a + b;
+    let addAnswer = `The sum of ${a} and ${b} is ${addedNumbers}.`;
+    return [addedNumbers, addAnswer];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -29,13 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  let secondQuestionNumbers = a * b;
-  let secondQuestionAnswer = `The product of ${a} and ${b} is ${secondQuestionNumbers}.`;
-  return [secondQuestionNumbers, secondQuestionAnswer];
+    let secondQuestionNumbers = a * b;
+    let secondQuestionAnswer = `The product of ${a} and ${b} is ${secondQuestionNumbers}.`;
+    return [secondQuestionNumbers, secondQuestionAnswer];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +52,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum1 = sum(a, b)[0];
+  console.log(sum1);
+  let sum2 = sum(sum1, c)[0];
+  console.log(sum2);
+  let multiply1 = multiply(a, b)[0];
+  console.log(multiply1);
+  let multiply2 = multiply(multiply1, c)[0];
+  console.log(multiply2);
+  let quest3Sum = `${a} and ${b} and ${c} sum to ${sum2}.`;
+  console.log(quest3Sum);
+  let quest3Mult = `The product of ${a} and ${b} and ${c} is ${multiply2}.`;
+  console.log(quest3Mult);
+  console.log([sum2, multiply2, quest3Sum, quest3Mult]);
+  return[sum2, multiply2, quest3Sum, quest3Mult];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
